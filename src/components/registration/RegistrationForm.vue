ed5bf07d369db37d1e8a4fbd25c1b80a1379d13a
nusgus <anton.mokhonko@outlook.com>
2019-09-10 14:09:06 +0300

    added vuetife styles

---
+++ src/components/registration/RegistrationForm.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  div.
+</template>
+
+<script>
+  export default {
+    name: "RegistrationForm"
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import 'vuetify/dist/vuetify.min.css'
 
 axios.defaults.baseURL = 'http://localhost:3000';
 
