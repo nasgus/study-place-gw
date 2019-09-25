212dd17c23836d3088fd56b5e1ce7de070b4eb39
nusgus <anton.mokhonko@outlook.com>
2019-09-25 15:57:04 +0300

    created header component

---
+++ src/views/Main.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div
+    Header
+    router-view
+</template>
+
+<script>
+  import Header from "./Header";
+
+  export default {
+    name: "Main",
+    components: {
+      Header
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
