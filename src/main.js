f23cb44cfe33b6e2aa0aa726de494c29740a0e29
nusgus <anton.mokhonko@outlook.com>
2019-09-30 12:43:41 +0300

    fix regisdtration componnet

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from '../src/router'
 import store from './store'
 import axios from 'axios'
-import 'bulma/css/bulma.min.css'
 
 axios.defaults.baseURL = 'http://localhost:3000';
 
