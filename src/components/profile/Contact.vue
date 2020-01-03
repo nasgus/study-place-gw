b615e2140918eb080bebb69c0ee12923ff50c6e2
nusgus <anton.mokhonko@outlook.com>
2020-01-03 11:31:39 +0200

    created list of contacts

---
+++ src/components/profile/Contact.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  v-card.my-2(max-width="400px")
+    v-list-item
+      v-list-item-avatar(color="grey")
+      v-list-item-content
+        v-list-item-title.headline {{fullName}}
+        v-list-item-subtitle {{education}}
+      v-list-item-action
+        v-layout
+          v-btn(icon)
+            v-icon mdi-phone-in-talk
+          v-btn(icon)
+            v-icon mdi-account
+</template>
+
+<script>
+  export default {
+    name: "Contact",
+    props: {
+      fullName: {
+        type: String,
+        required: true
+      },
+      education: {
+        type: String,
+        required: true
+      },
+      userId: {
+        type: String,
+        required: true
+      },
+      photo: {
+        type: String
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -5,19 +5,26 @@
         v-btn(icon)
           v-icon mdi-account-edit
     v-layout()
-      v-flex(lg3, xl3)
-        div.user-name {{fullName}}
+      v-flex(lg4, xl4)
+        h4.user-name {{fullName}}
         v-img.user-photo.mt-5(:src="require('../assets/profile-photo.jpg')")
-      v-flex
-        div.mb-5 Информация
+      v-flex(lg4, xl4)
+        h4.mb-5 Информация
         v-input.information-input(v-for="(input, index) in list", :key="index", :messages="[input.subtitle]") {{profile[input.key]}}
+      v-flex(xl4, lg4)
+        h4 Список контактов
+        div.contacts
+          Contact(v-for="(user, index) in users", :fullName="user.fullName", :education="user.education", :userId="user.userId", :key="index")
+
 </template>
 
 <script>
   import api from '../api'
+  import Contact from "../components/profile/Contact";
 
   export default {
     name: "Profile",
+    components: {Contact},
     data() {
       return {
         list: [
@@ -33,6 +40,33 @@
             key: 'phone',
             subtitle: 'номер телефона'
           }
+        ],
+        users: [
+          {
+            fullName: 'Anton Mokhonko', education: 'NTU KHPI', userId: '2', photo: ''
+          },
+          {
+            fullName: 'Anton Mokhonko', education: 'NTU KHPI', userId: '2', photo: ''
+          },
+          {
+            fullName: 'Anton Mokhonko', education: 'NTU KHPI', userId: '2', photo: ''
+          },
+          {
+            fullName: 'Anton Mokhonko', education: 'NTU KHPI', userId: '2', photo: ''
+          },
+          {
+            fullName: 'Anton Mokhonko', education: 'NTU KHPI', userId: '2', photo: ''
+          },
+          {
+            fullName: 'Anton Mokhonko', education: 'NTU KHPI', userId: '2', photo: ''
+          },
+          {
+            fullName: 'Anton Mokhonko', education: 'NTU KHPI', userId: '2', photo: ''
+          },
+          {
+            fullName: 'Anton Mokhonko', education: 'NTU KHPI', userId: '2', photo: ''
+          },
+
         ]
       }
     },
@@ -62,6 +96,13 @@
     height: 300px;
     width: 300px;
     border-radius: 10px;
+  }
+
+  .contacts {
+    max-width: 420px;
+    overflow: auto;
+    max-height: 500px;
+    padding: 5px;
   }
 
   .v-input__slot {
