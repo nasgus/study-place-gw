92d14e94c5954a8386c410ef7829d44c1296af05
Anton Mokhonko <anton.mokhonko@qpard.com>
2020-01-22 17:22:46 +0200

    ADD: throttling

--- src/main.js
+++ src/main.js
@@ -4,14 +4,21 @@ import router from '../src/router'
 import store from './store'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-import 'vue-trix'
 import '@mdi/font/css/materialdesignicons.css'
 import VueSocketIO from 'vue-socket.io'
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import VueLodash from 'vue-lodash'
 
 let vuetify = new Vuetify();
 
+Vue.use(VueQuillEditor);
+Vue.use(VueLodash)
+
 Vue.use(new VueSocketIO({
-  debug: true,
+  debug: false,
   connection: process.env.VUE_APP_API_URL,
 }));
 
--- src/views/Lesson.vue
+++ src/views/Lesson.vue
@@ -10,41 +10,44 @@
 
       v-flex(xl12, lg12)
         v-flex(xl10, lg10)
-          VueTrix.text-editor(v-model="text", height="500px", @change="sendNotebook")
+          quill-editor(v-model="text", @change="onEditNotebook")
 </template>
 
 <script>
-  import VueTrix from 'vue-trix'
   import api from '../api'
 
 
   export default {
     name: "Lesson",
-    components: {
-      VueTrix
-    },
+    components: {},
     props: {
       contactId: [Number, String]
     },
     data() {
       return {
-        text: ''
+        text: '',
+        oldText: ''
       }
     },
     methods: {
       sendNotebook(e) {
         console.log(e)
-        // this.$socket.emit('notebook', txt, this.userId, this.$route.query.contactId)
 
       },
-      onEditNotebook() {
-        this.$socket.emit('send-notebook-text', this.text);
-        // console.log(txt)
-      }
+      // onEditNotebook(ev) {
+      //   return this._.debounce(() => {
+      //     this.$socket.emit('send-notebook-text', ev.html, this.$route.params.lessonId);
+      //   }, 500)
+      // }
     },
     computed: {
       userId() {
         return this.$store.getters.userId
+      },
+      onEditNotebook () {
+        return this._.debounce((ev) => {
+          this.$socket.emit('send-notebook-text', ev.html, this.$route.params.lessonId);
+        }, 500)
       }
     },
     sockets: {
@@ -54,12 +57,7 @@
     },
     mounted() {
       this.$socket.emit('join', this.$route.params.lessonId, this.userId);
-      // api.post('/lessons/connect', {uniqueLessonId: this.$route.params.lessonId})
-      //   .then(res => {
-      //   });
-    },
-    mounted() {
-      setInterval(this.onEditNotebook, 500)
+      // setInterval(this.onEditNotebook, 750)
     }
   }
 </script>
